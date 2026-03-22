c6c61f8f26faa7967fb3fb7abb3b60ba52e1b416
NirmalkumarS2002 <nirmalsiva555@gmail.com>
2026-03-23 00:28:23 +0530

    Project Final

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -14,7 +14,6 @@ export default function Navbar() {
         <div className='NavMain'>
             <BrowserRouter>
        <div className="Navagation">
-
     <div className='pro'>
     <Profile/>
     </div>
